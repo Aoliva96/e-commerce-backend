df750af84cdb2bb240dcf7961376e2a98552be30
Aster Oliva <asterthealabaster@gmail.com>
2024-05-17 01:17:04 -0700

    added sequelize sync to server.js

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const routes = require("./routes");
-// import sequelize connection
+const sequelize = require("./config/connection");
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const DB_NAME = process.env.DB_NAME;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+// Sync sequelize models to db, then start server
+console.log(
+  `\x1b[32m[sequelize] attempting sync to database \`${DB_NAME}\`\x1b[0m`
+);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () =>
+    console.log(
+      `\x1b[32m[Sync successful]\n[App listening on PORT:${PORT}]\x1b[0m`
+    )
+  );
 });
